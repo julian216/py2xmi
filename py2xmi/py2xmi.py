adec4192f6bf7866f82d649e219e86bcfca0ae30
julian216 <julian_no_spam@cantab.net>
2013-08-31 18:37:57 +0100

    added initial top level classes

---
+++ py2xmi/py2xmi.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 31, 2013
+
+@author: julian
+'''
+
+class py2xmi(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
